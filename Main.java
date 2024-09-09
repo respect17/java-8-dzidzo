56faa7424176c128bcc89deb4b3ad19c8388690b
Respect Murimi <respectmurimi2000@gmail.com>
2024-09-09 11:43:17 +0200

    Add variables to a class

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 public class Main {
+    String name;
+    int age;
 
     public static void main(String args[]) {
-        System.out.println("Good!");
+        int age = 33;
+        String name = "Melody";
+        System.out.println("My name is " + name);
+        System.out.println("i am " + age + " years old ");
 
     }
 
